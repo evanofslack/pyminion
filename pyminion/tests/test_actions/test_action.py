be071f2c317e9aa2c3ebaf7dd93a7a846dbed5df
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-18 01:34:01 -0400

    added base cards

---
+++ pyminion/tests/test_actions/test_action.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pyminion.models.base import Turn, Player
+from pyminion.models.cards import Action
+from pyminion.base_set.base_cards import smithy
+from pyminion.exceptions import InsufficientActions
+
+action = Action(name="test", cost="0")
+
+
+def test_action_common_play(turn: Turn, player: Player):
+
+    player.hand.add(action)
+    assert len(player.hand) == 1
+    player.hand.cards[0].common_play(turn, player)
+    assert len(player.hand) == 0
+    assert len(player.playmat) == 1
+    assert type(player.playmat.cards[0]) is Action
+    assert turn.actions == 0
+
+
+def test_action_draw_no_actions(turn: Turn, player: Player):
+    player.hand.add(action)
+    player.hand.cards[0].common_play(turn, player)
+    player.hand.add(action)
+
+    with pytest.raises(InsufficientActions):
+        player.hand.cards[-1].common_play(turn, player)
