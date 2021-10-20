68fbd3e2ed97208cf2613a68c13ba36da38a590b
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-19 20:19:46 -0400

    rename constants

--- pyminion/expansions/base.py
+++ pyminion/expansions/base.py
@@ -16,14 +16,14 @@ from pyminion.models.base import (
 from pyminion.models.core import Pile, Card
 from typing import List
 
-COPPER = 60
-SILVER = 40
-GOLD = 30
-VICTORY = 8
+COPPER_PILE = 60
+SILVER_PILE = 40
+GOLD_PILE = 30
+VICTORY_PILE = 8
 
 START_COPPER = 7
 START_ESTATE = 3
-PILE_LENGTH = 10
+KINGDOM_PILE = 10
 
 
 def pile_maker(card: Card, num_card: int) -> Pile:
@@ -31,15 +31,15 @@ def pile_maker(card: Card, num_card: int) -> Pile:
 
 
 def kingdom_maker(cards: List[Card]) -> List[Pile]:
-    return [pile_maker(card, PILE_LENGTH) for card in cards]
+    return [pile_maker(card, KINGDOM_PILE) for card in cards]
 
 
-copper_pile = pile_maker(card=copper, num_card=COPPER)
-silver_pile = pile_maker(card=silver, num_card=SILVER)
-gold_pile = pile_maker(card=gold, num_card=GOLD)
-estate_pile = pile_maker(card=estate, num_card=VICTORY)
-duchy_pile = pile_maker(card=duchy, num_card=VICTORY)
-province_pile = pile_maker(card=province, num_card=VICTORY)
+copper_pile = pile_maker(card=copper, num_card=COPPER_PILE)
+silver_pile = pile_maker(card=silver, num_card=SILVER_PILE)
+gold_pile = pile_maker(card=gold, num_card=GOLD_PILE)
+estate_pile = pile_maker(card=estate, num_card=VICTORY_PILE)
+duchy_pile = pile_maker(card=duchy, num_card=VICTORY_PILE)
+province_pile = pile_maker(card=province, num_card=VICTORY_PILE)
 
 
 core_supply = [
