2aa67662bed0e4f87f4d790f5094cc253fd3ac38
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-20 22:18:24 -0400

    add multiple discard input

--- pyminion/util.py
+++ pyminion/util.py
@@ -1,6 +1,8 @@
 from pyminion.models.core import Card, Pile
 from pyminion.exceptions import InvalidBinaryInput
+
 from typing import List
+from collections import Counter
 
 # TODO write tests
 def pile_maker(card: Card, num_card: int) -> Pile:
@@ -26,3 +28,19 @@ def binary_decision(prompt: str) -> bool:
                 )
         except InvalidBinaryInput as e:
             print(e)
+
+
+def multiple_discard_decision(prompt: str, valid_cards: List[Card]) -> List[Card]:
+    discard_input = input(prompt)
+    discard_strings = [x.strip() for x in discard_input.split(",")]
+    discard_cards = []
+    for discard_string in discard_strings:
+        for card in valid_cards:
+            if discard_string == card.name:
+                discard_cards.append(card)
+                break
+
+    discard_count = Counter(discard_cards)
+    valid_count = Counter(valid_cards)
+
+    return discard_cards
