897a50f13a1c75e0c60f9213aee67739fc294a7f
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-27 01:23:58 -0400

    add basic bots

---
+++ pyminion/bots/bm_smithy.py
@@ -0,0 +1,34 @@
+from pyminion.models.core import Player, Deck, Game, 
+from pyminion.expansions.base import silver, gold, province, smithy
+
+
+class BM_Smithy(Player):
+    def __init__(
+        self,
+        deck: Deck,
+        player_id: str = "bm_smithy",
+    ):
+        super().__init__(deck=deck, player_id=player_id)
+
+    def take_turn(self, game: Game):
+
+        self.start_turn()
+        print(f"bot turns: {self.turns}")
+
+        try:
+            self.play(smithy, game)
+            print("bot played smithy")
+        except:
+            pass
+
+        self.autoplay_treasures()
+        print(f"bot money: {self.state.money}")
+        if self.state.money >= 8:
+            self.buy(province, game.supply)
+        elif self.state.money >= 6:
+            self.buy(gold, game.supply)
+        elif self.state.money >= 4:
+            self.buy(smithy, game.supply)
+        elif self.state.money >= 3:
+            self.buy(silver, game.supply)
+        self.cleanup()
--- pyminion/main.py
+++ pyminion/main.py
@@ -11,46 +11,41 @@ from pyminion.expansions.base import (
     kingdom_cards,
 )
 from pyminion.models.base import (
-    estate,
     silver,
-    moneylender,
-    cellar,
-    chapel,
-    copper,
     gold,
     province,
 )
+from pyminion.bots.big_money import BigMoney
+from pyminion.bots.bm_smithy import BM_Smithy
 
 
 player = Player(
     deck=Deck(start_cards),
     player_id="player",
 )
+bm = BigMoney(deck=Deck(start_cards))
+bm_smithy = BM_Smithy(deck=Deck(start_cards))
+
 supply = Supply(piles=core_supply + kingdom_cards)
 trash = Trash()
-game = Game(players=[player], supply=supply, trash=trash)
+game = Game(players=[bm, bm_smithy], supply=supply, trash=trash)
 
 if __name__ == "__main__":
-    player.deck.shuffle()
-    player.draw(5)
 
+    bm.deck.shuffle()
+    bm.draw(5)
+    bm_smithy.deck.shuffle()
+    bm_smithy.draw(5)
     while not game.is_over():
-        player.start_turn()
-        print(f"turns: {player.turns}")
-        print(f"hand: {player.hand}")
 
-        player.autoplay_treasures()
-        print(f"money: {player.state.money}")
-        if player.state.money >= 8:
-            player.buy(province, supply)
-        elif player.state.money >= 6:
-            player.buy(gold, supply)
-        elif player.state.money >= 3:
-            player.buy(silver, supply)
-        player.cleanup()
+        bm.take_turn(game)
+        bm_smithy.take_turn(game)
+
+    winner = game.get_winner()
+    print(winner.player_id)
 
     """
-    with StringIO('asdf') as f:
+    with StringIO('yup') as f:
     stdin = sys.stdin
     sys.stdin = f
     print("'" + input() + "' wasn't actually typed at the command line")
