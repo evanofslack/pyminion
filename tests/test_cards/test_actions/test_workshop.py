f72070ac6d397066b1a9ef7ba9ca9dfe8cdeac25
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-27 23:45:58 -0400

    test workshop

--- tests/test_cards/test_actions/test_workshop.py
+++ tests/test_cards/test_actions/test_workshop.py
@@ -7,10 +7,11 @@ def test_workshop_gain_valid(player: Player, game: Game, monkeypatch):
     assert len(player.discard_pile) == 0
 
     # mock decision = input() as "Copper" to discard
-    monkeypatch.setattr("builtins.input", lambda _: "Copper")
+    monkeypatch.setattr("builtins.input", lambda _: "Estate")
 
     player.hand.cards[0].play(player, game)
     assert len(player.playmat) == 1
     assert len(player.discard_pile) == 1
     assert player.state.actions == 0
-    assert type(player.discard_pile.cards[0]) is Copper
+    assert type(player.discard_pile.cards[0]) is Estate
+    assert len(game.supply.piles[0]) == 7
