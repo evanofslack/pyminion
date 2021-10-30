160d279479f853391706cb31cd2bce5a59f3b77a
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-30 16:00:14 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyminion',
+   version='0.1.1',
+   description='Dominion but make it python',
+   author='Evan Slack',
+   author_email='evan.slack@outlook.com',
+   packages=['pyminion'],  
+   install_requires=[],
+)
