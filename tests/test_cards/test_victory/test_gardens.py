9d1ae6ab63596a56c12d93cb601f5f580a4e74fa
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-30 11:37:16 -0400

    minor additions to tests

--- tests/test_cards/test_victory/test_gardens.py
+++ tests/test_cards/test_victory/test_gardens.py
@@ -4,6 +4,7 @@ from pyminion.models.base import gardens, copper
 
 def test_gardens_score_10_cards(player: Player):
     player.hand.add(gardens)
+    assert len(player.deck) == 10
     player.deck.remove(copper)
     assert len(player.get_all_cards()) == 10
     assert player.hand.cards[0].score(player) == 1
