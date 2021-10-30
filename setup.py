0f543891961d01b75e61180da06dcbeb5a14aff5
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-10-30 16:04:11 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+NAME = "pyminion"
+VERSION = "0.1.1"
+DESCRIPTION = "Dominion but make it python"
+URL = "https://github.com/evanofslack/pyminion"
+AUTHOR = "Evan Slack"
+AUTHOR_EMAIL = "evan.slack@outlook.com"
+LICENSE = "MIT"
+
+
 setup(
-   name='pyminion',
-   version='0.1.1',
-   description='Dominion but make it python',
-   author='Evan Slack',
-   author_email='evan.slack@outlook.com',
-   packages=['pyminion'],  
-   install_requires=[],
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    packages=["pyminion"],
+    install_requires=[],
 )
