ce7b5fdc4d300dd4f63ab121560e182dcdc67d3c
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-05 19:39:24 -0400

    Create validation functions

---
+++ pyminion/validations.py
@@ -0,0 +1,33 @@
+from pyminion.models.core import Card
+from pyminion.exceptions import InvalidSingleCardInput, InvalidMultiCardInput
+from typing import List
+from collections import Counter
+
+
+def single_card_validation(target_card: Card, valid_cards: List[Card]) -> bool:
+    if target_card in valid_cards:
+        return True
+
+    raise InvalidSingleCardInput(
+        f"Invalid input, {target_card} is not a valid selection"
+    )
+
+
+def multiple_card_validation(target_cards: List[Card], valid_cards: List[Card]) -> bool:
+
+    for target_card in target_cards:
+        if target_card not in valid_cards:
+            raise InvalidMultiCardInput(
+                f"Invalid input, {target_card} is not a valid card"
+            )
+
+    target_count = Counter(target_cards)
+    valid_count = Counter(valid_cards)
+
+    for element in target_count:
+        if target_count[element] > valid_count[element]:
+            raise InvalidMultiCardInput(
+                f"Invalid input, attempted to drop too many copies of {element}"
+            )
+
+    return True
