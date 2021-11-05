3d8388aefdc368fd5d7875cc09176ce792e5d61c
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-04 22:21:36 -0400

    adjust simulation

--- pyminion/examples/simulation.py
+++ pyminion/examples/simulation.py
@@ -1,6 +1,28 @@
 from pyminion.game import Game
 from pyminion.simulator import Simulator
-from pyminion.examples.bot_game import game
+from pyminion.expansions.base import (
+    start_cards,
+    base_cards,
+    basic_cards,
+)
+from pyminion.players import BigMoney, Human
+
+human = Human()
+bot_1 = BigMoney(player_id="Bot 1")
+bot_2 = BigMoney(player_id="Bot 2")
+bot_3 = BigMoney(player_id="Bot 3")
+bot_4 = BigMoney(player_id="Bot 4")
+
+players = [bot_1, bot_2]
+expansions = [base_cards]
+
+
+game = Game(
+    players,
+    expansions,
+    basic_cards,
+    start_cards,
+)
 
 sim = Simulator(game, iterations=100)
 
