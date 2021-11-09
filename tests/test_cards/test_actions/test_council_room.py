e4c96f2d87906097e092c4a23d83b8d84cbeeaaa
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-08 21:54:13 -0500

    add moat, witch, and council room cards

---
+++ tests/test_cards/test_actions/test_council_room.py
@@ -0,0 +1,21 @@
+from pyminion.game import Game
+from pyminion.models.base import CouncilRoom, council_room
+
+
+def test_council_room(multiplayer_game: Game):
+    player = multiplayer_game.players[0]
+    player.hand.add(council_room)
+    assert len(player.hand) == 6
+    for p in multiplayer_game.players:
+        if p is not player:
+            assert len(p.hand) == 5
+    player.hand.cards[-1].play(player, multiplayer_game)
+    for p in multiplayer_game.players:
+        if p is not player:
+            assert len(p.hand) == 6
+    assert len(player.hand) == 9
+    assert len(player.playmat) == 1
+    assert type(player.playmat.cards[0]) is CouncilRoom
+    assert player.state.actions == 0
+    assert player.state.money == 0
+    assert player.state.buys == 2
---
+++ tests/test_cards/test_actions/test_witch.py
@@ -0,0 +1,22 @@
+from pyminion.game import Game
+from pyminion.models.base import Witch, witch, curse
+
+
+def test_witch(multiplayer_game: Game):
+    player = multiplayer_game.players[0]
+    player.hand.add(witch)
+    assert len(player.hand) == 6
+    for p in multiplayer_game.players:
+        if p is not player:
+            assert len(p.discard_pile) == 0
+    player.hand.cards[-1].play(player, multiplayer_game)
+    for p in multiplayer_game.players:
+        if p is not player:
+            assert len(p.discard_pile) == 1
+            assert p.discard_pile.cards[-1] is curse
+    assert len(player.hand) == 7
+    assert len(player.playmat) == 1
+    assert type(player.playmat.cards[0]) is Witch
+    assert player.state.actions == 0
+    assert player.state.money == 0
+    assert player.state.buys == 1
