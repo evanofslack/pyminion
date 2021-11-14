8939ad703e2c12d0ba5dc0658654f573a8804dec
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-14 11:16:41 -0500

    add more tests for bandit

--- tests/test_cards/test_actions/test_bandit.py
+++ tests/test_cards/test_actions/test_bandit.py
@@ -1,5 +1,5 @@
 from pyminion.game import Game
-from pyminion.models.base import Bandit, Gold, bandit, copper, gold, silver
+from pyminion.models.base import Bandit, Gold, Silver, bandit, copper, gold, silver
 
 
 def test_bandit_gains_gold(multiplayer_game: Game):
@@ -19,21 +19,39 @@ def test_bandit_gains_gold(multiplayer_game: Game):
 
 def test_bandit_trash_one_silver(multiplayer_game: Game):
     player = multiplayer_game.players[0]
+    opponent = multiplayer_game.players[1]
     player.hand.add(bandit)
-    assert len(player.hand) == 6
-    # for p in multiplayer_game.players:
-    #     if p is not player:
-    #         assert len(p.discard_pile) == 0
+    opponent.deck.add(silver)
+    assert len(opponent.discard_pile) == 0
+    assert len(multiplayer_game.trash) == 0
     player.hand.cards[-1].play(player, multiplayer_game)
-    # for p in multiplayer_game.players:
-    #     if p is not player:
-    #         assert len(p.discard_pile) == 1
-    #         assert p.discard_pile.cards[-1] is curse
-    assert len(player.hand) == 5
-    assert len(player.playmat) == 1
-    assert type(player.playmat.cards[0]) is Bandit
-    assert len(player.discard_pile) == 1
-    assert type(player.discard_pile.cards[0]) is Gold
-    assert player.state.actions == 0
-    assert player.state.money == 0
-    assert player.state.buys == 1
+    assert len(multiplayer_game.trash) == 1
+    assert type(multiplayer_game.trash.cards[0]) is Silver
+    assert len(opponent.discard_pile) == 1
+
+
+def test_bandit_trash_silver_over_gold(multiplayer_game: Game):
+    player = multiplayer_game.players[0]
+    opponent = multiplayer_game.players[1]
+    player.hand.add(bandit)
+    opponent.deck.add(silver)
+    opponent.deck.add(gold)
+    assert len(opponent.discard_pile) == 0
+    assert len(multiplayer_game.trash) == 0
+    player.hand.cards[-1].play(player, multiplayer_game)
+    assert len(multiplayer_game.trash) == 1
+    assert type(multiplayer_game.trash.cards[0]) is Silver
+    assert len(opponent.discard_pile) == 1
+    assert type(opponent.discard_pile.cards[0]) is Gold
+
+
+def test_bandit_discard_two_non_treasure(multiplayer_game: Game):
+    player = multiplayer_game.players[0]
+    opponent = multiplayer_game.players[1]
+    player.hand.add(bandit)
+    opponent.deck.add(copper)
+    assert len(opponent.discard_pile) == 0
+    assert len(multiplayer_game.trash) == 0
+    player.hand.cards[-1].play(player, multiplayer_game)
+    assert len(multiplayer_game.trash) == 0
+    assert len(opponent.discard_pile) == 2
