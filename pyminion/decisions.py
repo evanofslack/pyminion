e1d6aa8ce1ba97a971b1985ebace6687fb59408f
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-20 16:29:27 -0500

    better docstrings for decisions

--- pyminion/decisions.py
+++ pyminion/decisions.py
@@ -41,77 +41,80 @@ def validate_input(
 
 def binary_decision(prompt: str) -> bool:
     """
-    Get user response to a binary "yes" or "no" question.
-    Raise exception is input is anything other than 'y' or 'n'
+    Get user response to a binary question.
+    Raise exception is input is anything other than 'yes' or 'no'
 
     """
 
     decision = input(prompt)
-    if decision == "y":
+    if decision in ("yes", "y", "Yes", "Y"):
         return True
-    elif decision == "n":
+    elif decision in ("no", "n", "No", "N"):
         return False
     else:
-        raise InvalidBinaryInput("Invalid response, valid choices are 'y' or 'n'")
+        raise InvalidBinaryInput("Invalid response, valid choices are 'yes' or 'no'")
 
 
 def single_card_decision(
     prompt: str, valid_cards: List[Card], valid_mixin: str = "placeholder"
 ) -> Optional[Union[Card, str]]:
     """
-    Get user response when given the option to select one card
+    Get user input when given the option to select one card
 
-    valid_mixin allows for options other than a card to be selected i.e. "auto"
-    to autoplay treasures when deciding treasures to play
+    valid_cards are a list of cards that a user can choose from. For example,
+    when prompting a user to discard a card from their hand, valid cards would
+    be the user's hand.
 
-    Raise exception if user provided selection is not valid.
+    valid_mixin allows for options other than a card to be selected. For example,
+    "all" mixin would be used to autoplay treasures when prompting treasures to play
+
+    Raise exception if user provided selection is not in valid_cards and is not
+    a valid_mixin.
 
     """
     card_input = input(prompt)
     if not card_input:
-        return False
+        return None
 
     if card_input == valid_mixin:
         return valid_mixin
 
-    selected_card = None
     for card in valid_cards:
         if card_input.casefold() == card.name.casefold():
-            selected_card = card
-            break
+            return card
 
-    if not selected_card:
-        raise InvalidSingleCardInput(
-            f"Invalid input, {card_input} is not a valid selection"
-        )
-
-    return selected_card
+    raise InvalidSingleCardInput(
+        f"Invalid input, {card_input} is not a valid selection"
+    )
 
 
 def multiple_card_decision(
     prompt: str, valid_cards: List[Card]
 ) -> Optional[List[Card]]:
     """
-    Get user response when given the option to select multiple cards
-    Raise exception if user provided selection is not valid.
+    Get user input when given the option to select multiple cards
+
+    valid_cards are a list of cards that a user can choose from. For example,
+    when prompting a user to trash cards from their hand, valid cards would
+    be the user's hand.
+
+    Raise exception if user provided selection is not in valid_cards.
 
     """
     card_input = input(prompt)
     if not card_input:
-        return
+        return None
     card_strings = [x.strip() for x in card_input.split(",")]
     selected_cards = []
     for card_string in card_strings:
         for card in valid_cards:
-            # compare strings regardless of case i.e. 'Copper' = 'copper'
             if card_string.casefold() == card.name.casefold():
                 selected_cards.append(card)
                 break
-
-    if not selected_cards:
-        raise InvalidMultiCardInput(
-            f"Invalid input, {card_strings[0]} is not a valid card"
-        )
+        else:
+            raise InvalidMultiCardInput(
+                f"Invalid input, {card_string} is not a valid card"
+            )
 
     selected_count = Counter(selected_cards)
     valid_count = Counter(valid_cards)
@@ -119,6 +122,6 @@ def multiple_card_decision(
     for element in selected_count:
         if selected_count[element] > valid_count[element]:
             raise InvalidMultiCardInput(
-                f"Invalid input, attempted to drop too many copies of {element}"
+                f"Invalid input, attempted to select too many copies of {element}"
             )
     return selected_cards
