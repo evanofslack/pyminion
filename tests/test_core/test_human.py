d856746299769e1603a88a2ba54aa9a9e2704fca
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-20 17:04:13 -0500

    add tests to human

--- tests/test_core/test_human.py
+++ tests/test_core/test_human.py
@@ -1,3 +1,5 @@
+from pyminion.expansions.base import copper, moat, witch
+from pyminion.game import Game
 from pyminion.players import Human
 
 
@@ -17,5 +19,93 @@ def test_human_cleanup(human: Human):
     assert len(human.playmat) == 0
     human.start_cleanup_phase()
     assert len(human.discard_pile) == 5
+    assert len(human.hand) == 5
+    assert len(human.playmat) == 0
+
+
+def test_yes_input(human: Human, monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "y")
+    assert human.binary_decision(prompt="test") is True
+    monkeypatch.setattr("builtins.input", lambda _: "yes")
+    assert human.binary_decision(prompt="test") is True
+
+
+def test_no_input(human: Human, monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "n")
+    assert human.binary_decision(prompt="test") is False
+    monkeypatch.setattr("builtins.input", lambda _: "no")
+    assert human.binary_decision(prompt="test") is False
+
+
+def test_is_attacked_no_moat(human: Human, monkeypatch):
+    assert human.is_attacked(player=None, attack_card=None)
+
+
+def test_is_attacked_yes_moat(human: Human, monkeypatch):
+    human.hand.add(card=moat)
+    monkeypatch.setattr("builtins.input", lambda _: "yes")
+    assert not human.is_attacked(player=human, attack_card=witch)
+
+
+def test_action_phase_no_actions(human: Human, game: Game):
+    assert not human.start_action_phase(game)
+
+
+def test_action_phase_no_input(human: Human, game: Game, monkeypatch):
+    human.hand.add(moat)
+    monkeypatch.setattr("builtins.input", lambda _: "")
+    assert not human.start_action_phase(game)
+
+
+def test_action_phase_play_action(human: Human, game: Game, monkeypatch):
+    human.hand.add(moat)
+    monkeypatch.setattr("builtins.input", lambda _: "moat")
+    human.start_action_phase(game)
+    assert len(human.playmat) == 1
+
+
+def test_treasure_phase_no_treasures(human: Human, game: Game, monkeypatch):
+    assert not human.start_treasure_phase(game)
+
+
+def test_treasure_phase_one_copper(human: Human, game: Game, monkeypatch):
+    human.hand.add(copper)
+    monkeypatch.setattr("builtins.input", lambda _: "copper")
+    human.start_treasure_phase(game)
+    assert human.state.money == 1
+    assert human.playmat.cards[0].name == "Copper"
+
+
+def test_treasure_phase_autoplay(human: Human, game: Game, monkeypatch):
+    for i in range(3):
+        human.hand.add(copper)
+    monkeypatch.setattr("builtins.input", lambda _: "all")
+    human.start_treasure_phase(game)
+    assert human.state.money == 3
+
+
+def test_buy_phase_no_buys(human: Human, game: Game):
+    human.state.buys = 0
+    assert not human.start_buy_phase(game)
+
+
+def test_buy_phase_no_input(human: Human, game: Game, monkeypatch):
+    human.state.money = 2
+    monkeypatch.setattr("builtins.input", lambda _: "")
+    assert not human.start_buy_phase(game)
+
+
+def test_buy_phase_input_estate(human: Human, game: Game, monkeypatch):
+    human.state.money = 2
+    monkeypatch.setattr("builtins.input", lambda _: "estate")
+    human.start_buy_phase(game)
+    assert human.state.money == 0
+    assert human.discard_pile.cards[-1].name == "Estate"
+
+
+def test_cleanup_phase(human: Human):
+    human.hand.add(copper)
+    human.playmat.add(copper)
+    human.start_cleanup_phase()
     assert len(human.hand) == 5
     assert len(human.playmat) == 0
