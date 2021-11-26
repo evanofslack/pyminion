0192bd9a928bb5106be68124a0b37984d2695374
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-11-25 22:31:55 -0500

    create optimized base bot

--- pyminion/bots/__init__.py
+++ pyminion/bots/__init__.py
@@ -1,3 +1,5 @@
-from pyminion.bots.base_bot import Bot
+from pyminion.bots.abstract import AbstractBot
+from pyminion.bots.base import Bot
+from pyminion.bots.base_optimized import OptimizedBot
 from pyminion.bots.big_money import BigMoney
 from pyminion.bots.big_money_ultimate import BigMoneyUltimate
