763d8669e5a2023d89cf5a174a0fb74cf6fb9c92
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-04 20:17:04 -0500

    improve game test coverage

--- tests/test_core/test_game.py
+++ tests/test_core/test_game.py
@@ -1,6 +1,10 @@
-from pyminion.core import Supply, Trash
-from pyminion.expansions.base import duchy, estate, gold, province
+import pytest
+from pyminion.bots import BigMoney, OptimizedBot
+from pyminion.core import Card, Supply, Trash
+from pyminion.exceptions import InvalidGameSetup, InvalidPlayerCount
+from pyminion.expansions.base import base_cards, duchy, estate, gold, province, smithy
 from pyminion.game import Game
+from pyminion.players import Human
 
 
 def test_game_fixture(game: Game):
@@ -10,8 +14,71 @@ def test_game_fixture(game: Game):
     assert not game.trash
 
 
+def test_game_too_many_players(human: Human):
+    with pytest.raises(InvalidPlayerCount):
+        game = Game(players=[human, human, human, human, human], expansions=None)
+
+
+def test_game_too_few_players():
+    with pytest.raises(InvalidPlayerCount):
+        game = Game(players=[], expansions=None)
+
+
 def test_game_create_supply(game: Game):
     assert len(game.supply.piles) == 17
+
+
+def test_setup_1_player(human: Human):
+    game = Game(players=[human], expansions=[base_cards])
+    game.supply = game._create_supply()
+    assert game.supply.pile_length(pile_name="Curse") == 10
+    assert game.supply.pile_length(pile_name="Province") == 5
+    assert game.supply.pile_length(pile_name="Copper") == 53
+
+
+def test_setup_2_players(human: Human):
+    game = Game(players=[human, human], expansions=[base_cards])
+    game.supply = game._create_supply()
+    assert game.supply.pile_length(pile_name="Curse") == 10
+    assert game.supply.pile_length(pile_name="Province") == 8
+    assert game.supply.pile_length(pile_name="Copper") == 46
+
+
+def test_setup_3_players(human: Human):
+    game = Game(players=[human, human, human], expansions=[base_cards])
+    game.supply = game._create_supply()
+    assert game.supply.pile_length(pile_name="Curse") == 20
+    assert game.supply.pile_length(pile_name="Province") == 12
+    assert game.supply.pile_length(pile_name="Copper") == 39
+
+
+def test_setup_4_players(human: Human):
+    game = Game(players=[human, human, human, human], expansions=[base_cards])
+    game.supply = game._create_supply()
+    assert game.supply.pile_length(pile_name="Curse") == 30
+    assert game.supply.pile_length(pile_name="Province") == 12
+    assert game.supply.pile_length(pile_name="Copper") == 32
+
+
+def test_setup_user_selected_card(human):
+    game = Game(
+        players=[human, human, human, human],
+        expansions=[base_cards],
+        kingdom_cards=[smithy],
+    )
+    game.supply = game._create_supply()
+    assert smithy in game.supply.avaliable_cards()
+
+
+def test_setup_invalid_user_selected_card(human):
+    fake_card = Card(name="fake", cost=0, type=("Action",))
+    game = Game(
+        players=[human, human, human, human],
+        expansions=[base_cards],
+        kingdom_cards=[fake_card],
+    )
+    with pytest.raises(InvalidGameSetup):
+        game.supply = game._create_supply()
 
 
 def test_game_is_over_false(game: Game):
@@ -53,3 +120,25 @@ def test_game_win(multiplayer_game: Game):
 def test_game_win_turns(multiplayer_game: Game):
     multiplayer_game.players[1].turns += 1
     assert multiplayer_game.get_winner() == multiplayer_game.players[0]
+
+
+def test_game_1_player_play(bm_bot: BigMoney):
+    game = Game(
+        players=[bm_bot],
+        expansions=[base_cards],
+        kingdom_cards=[smithy],
+        use_logger=False,
+    )
+    game.play()
+    assert game.get_winner() == bm_bot
+
+
+def test_game_2_player_play(bm_bot: BigMoney):
+    game = Game(
+        players=[bm_bot, bm_bot],
+        expansions=[base_cards],
+        kingdom_cards=[smithy],
+        use_logger=False,
+    )
+    game.play()
+    game.get_winner()
