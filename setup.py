490a9421bccc0816764e687325b35835bcbb284b
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-12-04 23:58:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyminion",
-    version="0.1.5",
+    version="0.2.0",
     author="Evan Slack",
     author_email="evan.slack@outlook.com",
     description="Dominion but make it python",
