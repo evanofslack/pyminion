82b4d6759691cfb2ac905623f97e0087efc41fbd
John Wilkes <jwilkes0011@gmail.com>
2023-04-15 18:43:37 -0500

    Adding another Swindler test

--- tests/test_cards/test_actions/test_swindler.py
+++ tests/test_cards/test_actions/test_swindler.py
@@ -1,4 +1,4 @@
-from pyminion.expansions.base import copper
+from pyminion.expansions.base import copper, moat
 from pyminion.expansions.intrigue import Swindler, swindler
 from pyminion.game import Game
 
@@ -20,4 +20,27 @@ def test_swindler(multiplayer_game: Game, monkeypatch):
     assert p1.state.actions == 0
     assert p1.state.money == 2
 
+    assert len(p2.discard_pile) > 0
     assert p2.discard_pile.cards[-1].name == "Curse"
+
+
+def test_swindler_moat(multiplayer_game: Game, monkeypatch):
+    players = multiplayer_game.players
+    p1 = players[0]
+    p2 = players[1]
+
+    p1.hand.add(swindler)
+    p2.hand.add(moat)
+    p2.deck.cards.insert(0, moat)
+
+    monkeypatch.setattr("builtins.input", lambda _: "y")
+
+    p1.play(swindler, multiplayer_game)
+    assert len(p1.hand) == 5
+    assert len(p1.playmat) == 1
+    assert type(p1.playmat.cards[0]) is Swindler
+    assert p1.state.actions == 0
+    assert p1.state.money == 2
+
+    assert p2.deck.cards[0].name == "Moat"
+    assert len(p2.discard_pile) == 0
