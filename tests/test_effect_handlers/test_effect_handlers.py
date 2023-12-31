80a3a22365c7d7342ae6f0bf808ddf67d56730ed
John Wilkes <jwilkes0011@gmail.com>
2023-12-30 18:27:47 -0600

    Adding simple test

---
+++ tests/test_effect_handlers/test_effect_handlers.py
@@ -0,0 +1,30 @@
+from pyminion.core import Card
+from pyminion.expansions.base import gold, smithy
+from pyminion.game import Game
+from pyminion.player import Player
+
+
+class HandlerTester:
+    def __init__(self):
+        self.handler_called = False
+
+    def handler(self, player: Player, card: Card, game: Game) -> None:
+        self.handler_called = True
+
+
+def test_on_gain(game: Game):
+    reg = game.event_registry
+
+    tester = HandlerTester()
+
+    reg.register_on_gain_handler(tester.handler)
+
+    player = game.players[0]
+    player.hand.add(gold)
+    player.hand.add(gold)
+    player.play(gold, game)
+    player.play(gold, game)
+
+    player.buy(smithy, game)
+
+    assert tester.handler_called
