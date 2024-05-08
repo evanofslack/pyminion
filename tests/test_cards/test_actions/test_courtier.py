92dc74b102d60ab12a46348e479b3232b9ff1bcb
John Wilkes <jwilkes0011@gmail.com>
2024-05-08 17:22:59 -0500

    Adding more tests

--- tests/test_cards/test_actions/test_courtier.py
+++ tests/test_cards/test_actions/test_courtier.py
@@ -57,3 +57,36 @@ def test_courtier_2_types_buy_gain_gold(human: Human, game: Game, monkeypatch):
     assert human.state.actions == 0
     assert human.state.buys == 2
     assert human.state.money == 0
+
+
+def test_courtier_empty_hand(human: Human, game: Game, monkeypatch):
+    human.hand.add(courtier)
+
+    responses = []
+    monkeypatch.setattr("builtins.input", lambda _: responses.pop(0))
+
+    human.play(courtier, game)
+    assert len(human.hand) == 0
+    assert len(human.playmat) == 1
+    assert type(human.playmat.cards[0]) is Courtier
+    assert len(human.discard_pile) == 0
+    assert human.state.actions == 0
+    assert human.state.buys == 1
+    assert human.state.money == 0
+
+
+def test_courtier_1_card_hand(human: Human, game: Game, monkeypatch):
+    human.hand.add(courtier)
+    human.hand.add(copper)
+
+    responses = ["1"]
+    monkeypatch.setattr("builtins.input", lambda _: responses.pop(0))
+
+    human.play(courtier, game)
+    assert len(human.hand) == 1
+    assert len(human.playmat) == 1
+    assert type(human.playmat.cards[0]) is Courtier
+    assert len(human.discard_pile) == 0
+    assert human.state.actions == 1
+    assert human.state.buys == 1
+    assert human.state.money == 0
