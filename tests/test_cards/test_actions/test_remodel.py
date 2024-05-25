ddc10fb916f209ec13ecef8b40f9700081ef6210
John Wilkes <jwilkes0011@gmail.com>
2024-05-25 12:23:24 -0500

    Adding more tests

--- tests/test_cards/test_actions/test_remodel.py
+++ tests/test_cards/test_actions/test_remodel.py
@@ -1,6 +1,8 @@
 from pyminion.expansions.base import copper, remodel
+from pyminion.expansions.alchemy import alchemy_set, apothecary, golem
 from pyminion.game import Game
 from pyminion.human import Human
+import pytest
 
 
 def test_remodel_gain_valid(human: Human, game: Game, monkeypatch):
@@ -49,3 +51,23 @@ def test_remodel_empty_hand(human: Human, game: Game):
     assert len(human.discard_pile) == 0
     assert len(game.trash) == 0
     assert human.state.actions == 0
+
+
+@pytest.mark.expansions([alchemy_set])
+@pytest.mark.kingdom_cards([apothecary, golem])
+def test_remodel_potion_cost(human: Human, game: Game, monkeypatch):
+    human.hand.add(apothecary)
+    human.hand.add(copper)
+    human.hand.add(remodel)
+    assert len(human.discard_pile) == 0
+    assert len(game.trash) == 0
+
+    responses = ["apothecary", "golem"]
+    monkeypatch.setattr("builtins.input", lambda _: responses.pop(0))
+
+    human.play(remodel, game)
+    assert len(human.playmat) == 1
+    assert len(human.discard_pile) == 1
+    assert human.state.actions == 0
+    assert human.discard_pile.cards[0].name == "Golem"
+    assert game.trash.cards[0].name == "Apothecary"
